d79db16713f289926b7db995b4c24c61bad48276
cchammout <chadiac@DESKTOP-HOAA51R>
2024-03-15 18:10:35 -0500

    Added RESTful API endpoints to main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
-
 import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
-
 from ml.data import apply_label, process_data
 from ml.model import inference, load_model
 
@@ -26,22 +24,20 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = # TODO: enter the path for the saved encoder 
+path = "model/encoder.pkl"
 encoder = load_model(path)
 
-path = # TODO: enter the path for the saved model 
+path = "model/model.pkl"
 model = load_model(path)
 
 # TODO: create a RESTful API using FastAPI
-app = # your code here
+app = FastAPI()
 
 # TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
-    # your code here
-    pass
-
+    return {"message": "Welcome to the ML API!"}
 
 # TODO: create a POST on a different path that does model inference
 @app.post("/data/")
@@ -64,11 +60,7 @@ async def post_inference(data: Data):
         "sex",
         "native-country",
     ]
-    data_processed, _, _, _ = process_data(
-        # your code here
-        # use data as data input
-        # use training = False
-        # do not need to pass lb as input
-    )
-    _inference = # your code here to predict the result using data_processed
+    data_processed, _, _, _ = process_data(data, categorical_features=cat_features, training=False)
+    _inference = inference(model, data_processed)
     return {"result": apply_label(_inference)}
+
